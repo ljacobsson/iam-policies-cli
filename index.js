e3a851a32605ee9bc6eb73644f7810f1b36e02b6
Lars Jacobsson <lars.jacobsson@mathem.se>
2020-02-27 13:39:25 +0100

    fix: upping maxEventListeners to avoid warnings on windows systems

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 #!/usr/bin/env node
 const program = require("commander");
 const inputWizard = require("./src/input-wizard");
+const events = require("events");
+
+events.EventEmitter.defaultMaxListeners = 1000;
 
 program.version("1.0.0", "-v, --vers", "output the current version");
 
 program
-.option(
-  "-t, --template <filename>",
-  "Template file name",
-  "serverless.template"
-)
-.option(
-  "-f, --format <JSON|YAML>",
-  "Output format",
-  "JSON"
-)
-.option(
-  "-o, --output <console|clipboard>",
-  "Policy output",
-  "console"
-)
-.action(async cmd => {
+  .option(
+    "-t, --template <filename>",
+    "Template file name",
+    "serverless.template"
+  )
+  .option("-f, --format <JSON|YAML>", "Output format", "JSON")
+  .option("-o, --output <console|clipboard>", "Policy output", "console")
+  .action(async cmd => {
     await inputWizard.start(cmd.template, cmd.format, cmd.output);
   });
 
 // eslint-disable-next-line no-undef
 program.parse(process.argv);
-
