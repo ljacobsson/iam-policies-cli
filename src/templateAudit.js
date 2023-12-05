3eb122851705d3d101b93e786523c20f591da265
ljacobsson <larsj82@gmail.com>
2023-12-05 22:18:26 +0100

    removing unused sdk

--- src/templateAudit.js
+++
@@ -1,9 +0,0 @@
-const yamlCfn = require("yaml-cfn");
-const AWS = require("aws-sdk");
-const cloudFormation = new AWS.CloudFormation();
-async function audit(template) {}
-
-module.exports = {
-  parse,
-  stringify
-};
